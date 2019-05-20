881c878489ad1abb7b8639f0aff0ba9eaaaea6c9
yesnt <kauavitor-@hotmail.com>
2019-05-20 04:59:33 -0300

    what is this

--- enemies.py
+++ enemies.py
@@ -2,3 +2,14 @@
 # depending on lvl, more and more enemies should be added
 # TODO: complete this at least as a placeholder
 enemies = { #i've been thinking, is object oriented programming bad?
+	1: {
+		1: {
+			"name": "bro you a noob",
+			"desc": "you still lvl1 lomao",
+			"hp": 1000,
+			"dmg": 0
+		}
+	}
+} #bro chill we havent added an xp system yet lmao
+
+
