3c63158d6c74616560fbbaf2199b6c1e424a4d2c
yeeeesnt <000yesnt@protonmail.ch>
2020-02-23 22:05:58 -0300

    fuck you if it doesnt work i dong give aq shit

--- enemies.py
+++ enemies.py
@@ -13,7 +13,7 @@ enemies = { # XP based combat is impossible at the moment
 
 def fight():
 	use_reserve = False
-	healing = { }
+	healing = ""
 	data = globals.data
 
 	# no guarantee these checks will work
@@ -21,11 +21,18 @@ def fight():
 		print("You have no weapons!")
 		print(data["bp"])
 		return
-	elif data["bp"]["primary_wpn"] == "" or data["bp"]["primary_wpn"]["healing"]:
+	if data["bp"]["primary_wpn"] == "":
+		if data["bp"]["reserve"]["healing"]:
+			print("You only have a healing weapon! (And you skipped the primary weapon! HOW)")
+			return
 		use_reserve = True
-		healing = data["bp"]["primary_wpn"]
-	elif data["bp"]["reserve"]["healing"]:
-		healing = data["bp"]["reserve"]
+	elif data["bp"]["primary_wpn"] != "":
+		if data["bp"]["primary_wpn"]["healing"] and data["bp"]["reserve"] == "":
+			raise Exception("what")
+	elif data["bp"]["reserve"] != "":
+		if data["bp"]["reserve"]["healing"]:
+			healing = data["bp"]["reserve"]
+
 
 	enemy = enemies[randint(1, len(enemies))]
 
@@ -34,6 +41,9 @@ def fight():
 		data["hp"] -= enemy["dmg"]
 		print("Enemy's turn: You received " + str(enemy["dmg"]) + " DMG")
 		print("You now have: " + str(data["hp"]) + " HP")
+		if data["hp"] <= 0:
+			print("dead lol")
+			raise
 		if use_reserve:
 			enemy["hp"] -= data["bp"]["reserve"]["dmg"]
 			print("Your turn: You dealt " + data["bp"]["reserve"]["dmg"] + " DMG")
@@ -42,14 +52,11 @@ def fight():
 			enemy["hp"] -= data["bp"]["primary_wpn"]["dmg"]
 			print("Your turn: You dealt " + str(data["bp"]["primary_wpn"]["dmg"]) + " DMG")
 			print("Now he has: " + str(enemy["hp"]) + " HP")
-		if data["hp"] <= 25:
+		if data["hp"] <= 25 and healing != "":
 			data["hp"] += healing["dmg"]
 			print("You gave yourself +" + str(healing["dmg"]) + " HP, now you have " + str(data["hp"]) + " HP")
 
 		if enemy["hp"] <= 0:
 			print("You won!")
 			break
-		elif data["hp"] <= 0:
-			print("fuck")
-			raise
 
